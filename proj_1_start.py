966eeed570e578da39e957714676a45e3f544c0c
CGrosh <cpg441@gmail.com>
2021-01-20 23:38:12 -0500

    editsssss

--- proj_1_start.py
+++ proj_1_start.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA 
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+# Currently not using the Illumination Arrays, will be using after the first model fit 
+
 # Load in the matlab data file 
 data = io.loadmat('data.mat')['face']
 
@@ -15,22 +17,14 @@ for i in range(400):
     labels.append(0)
     labels.append(1)
 
-# Breaking out 3 different face types 
+# Seperate the indicies of the illumination images 
 illum_arr = np.array([i*3-1 for i in range(1,201)])
 other_arr = np.array([i for i in range(600)])
 neut_emote_indx = other_arr[np.isin(other_arr, illum_arr, invert=True)]
-# Use isin to segent the indicies
 
-illum_arrs = np.array([i*3 for i in range(200)])
 full_data = np.array([data[:,:,i] for i in range(600)])
 neut_emote = np.take(full_data, neut_emote_indx, axis=0)
-# print(neut_emote.shape)
-# plt.imshow(neut_emote[-1], cmap='gray')
-# plt.show()
 
-print(full_data.shape)
-print(neut_emote.shape)
-print(illum_arrs.shape)
 
 # Split data between Neutral, Expression, and Illumination images
 # neut_faces = data[:,:,0::3]
@@ -42,16 +36,15 @@ print(illum_arrs.shape)
 # exp_face = np.array([exp_faces[:,:,i] for i in range(200)])
 # illum_face = np.array([illum_faces[:,:,i] for i in range(200)])
 
-# Concatenate the Neutral and Expression Images 
-
-
 # Define PCA pipeline
-# pca = PCA(n_components=2)
+pca = PCA(n_components=2)
 
-# pca_neut = pca.fit_transform(neut_faces.reshape(200, 504))
+pca_neut = pca.fit_transform(neut_emote.reshape(400, 504))
 
-# pca_df = pd.DataFrame(data=pca_neut, columns=['PCA1', 'PCA2'])
-# print(pca_df)
+pca_df = pd.DataFrame(data=pca_neut, columns=['PCA1', 'PCA2'])
+pca_fd = pca_df.sample(frac=1).reset_index(drop=True)
+pca_df['labels'] = pd.Series(labels)
+print(pca_df.head())
 
 # plt.imshow(illum_face[0], cmap='gray')
 # plt.show()
